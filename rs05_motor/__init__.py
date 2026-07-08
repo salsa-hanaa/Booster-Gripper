0b3cb5b76726fe56aeffb024cd5414488e15b379
Lntr_tgrs <salsabilahanaadniah25@gmail.com>
2026-07-08 16:29:11 +0700

    refactor: remove deprecated example and test files for RS05Motor

--- rs05_motor/__init__.py
+++
@@ -1,4 +0,0 @@
-from .core import RS05Motor
-from .conversion import POS_RANGE, VEL_RANGE, KP_RANGE, KD_RANGE, TORQUE_RANGE
-
-__all__ = ["RS05Motor", "POS_RANGE", "VEL_RANGE", "KP_RANGE", "KD_RANGE", "TORQUE_RANGE"]
